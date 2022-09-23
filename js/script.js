12a9b59d739318bfc4a305bc41ade6915a1a8465
Roma Kulik <kulikr.js@mail.ru>
2022-09-23 14:22:46 +0300

    new project

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+  hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+  });
+  closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+  const counters = document.querySelectorAll('.skills__rating-counter'),
+    lines = document.querySelectorAll('.skills__bar__item-footer div');
+
+  counters.forEach((item, i) => {
+    lines[i].style.width = item.textContent;
+  })
+
+
+});
