8fec139b92c8468d41e1f93d5808b0e1b6703a88
Roma Kulik <kulikr.js@mail.ru>
2022-10-04 20:26:14 +0300

    fixed script to close menu & some change html

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Variables
   const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+    closeElem = document.querySelector('.menu__close'),
+    counters = document.querySelectorAll('.skills__rating-counter'),
+    lines = document.querySelectorAll('.skills__bar__item-footer div'),
+    overlay = document.querySelector('.menu__overlay');
 
+  /**
+   * Open hamburger menu
+   */
   hamburger.addEventListener('click', () => {
     menu.classList.add('active');
   });
+
+  /**
+   * Close menu, click on closeElem
+   */
   closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
   });
 
-  const counters = document.querySelectorAll('.skills__rating-counter'),
-    lines = document.querySelectorAll('.skills__bar__item-footer div');
+  /**
+   * Close menu, click on overlay
+   */
+  overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+  })
+
 
   counters.forEach((item, i) => {
     lines[i].style.width = item.textContent;
